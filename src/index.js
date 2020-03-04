997a1e6e5464cc46e10eb494a7e02825d1c18198
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-03-04 12:33:28 +0500

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openings = {
+        "(" : ")",
+        "{" : "}",
+        "[" : ']',
+        "1" : "2",
+        "3" : "4",
+        "5" : "6",
+    };
+    let closings = [")", "}", "]", "2", "4", "6"];
+    let bracketStack = [];
+
+    for(let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        if (openings[char] || char === '|' || char === "7" || char === '8') {
+            if (char === '|' || char === "7" || char === '8') {
+                if (bracketStack[bracketStack.length - 1] === char) {
+                    bracketStack.pop();
+                } else {
+                    bracketStack.push(char);
+                }
+            } else {
+                bracketStack.push(char);
+            }
+
+        } else if (closings.includes(char)) {
+            if (bracketStack.length === 0 || (openings[bracketStack.pop()] !== char)) {
+                return false;
+            }
+        }
+    }
+    return bracketStack.length === 0;
+};
